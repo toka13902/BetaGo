9e02982b23f3bd7bad03bd9800dd6696dcd77bac
U-ATHENAHEALTH\sapatel <sapatel@ATH018992.corp.athenahealth.com>
2017-06-16 18:20:56 -0400

    some bullshit commit

--- app/src/main/java/com/somestupidapp/betago/MainActivity.java
+++ app/src/main/java/com/somestupidapp/betago/MainActivity.java
@@ -2,6 +2,7 @@ package com.somestupidapp.betago;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView bsTv = (TextView)findViewById(R.id.somebullshitid);
+        bsTv.setText("some bullshit test");
     }
 }
